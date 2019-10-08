8c95bdce87567ad7024b55ee919230bd22289948
NikoThor <nikolajdk1@gmail.com>
2019-10-08 11:50:59 +0200

    Initial commit

---
+++ src/TestSuite.java
@@ -0,0 +1,14 @@
+public abstract class TestSuite {
+
+
+    public void runTestSuite() {
+        // TODO
+    }
+
+    protected abstract void testList();
+
+
+    public void assertEquals(int expected, int actual) {
+        //TODO
+    }
+}
---
+++ test/TestClass.java
@@ -0,0 +1,14 @@
+public class TestClass extends TestSuite {
+    @Override
+    protected void testList() {
+        testThis();
+        tesThat();
+    }
+
+    public void testThis(){
+        //TODO
+    }
+    public void tesThat(){
+        //TODO
+    }
+}
