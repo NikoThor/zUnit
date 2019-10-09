7f4addbf6107954ad0ba0995478ada0dd194c0d9
NikoThor <nikolajdk1@gmail.com>
2019-10-09 14:37:45 +0200

    Zunit 0.1

--- src/TestSuite.java
+++ src/TestSuite.java
@@ -1,14 +1,45 @@
 public abstract class TestSuite {
 
+    private int passed;
+    private int failed;
 
     public void runTestSuite() {
-        // TODO
+        passed = 0;
+        failed = 0;
+
+        testList();
+
+
+        System.out.println("Tests passed: " + passed);
+        System.out.println("Tests failed: " + failed);
     }
 
     protected abstract void testList();
 
 
     public void assertEquals(int expected, int actual) {
-        //TODO
+        if(expected == actual) {
+            passed++;
+        } else {
+            failed++;
+            System.out.println("Expected " + expected + " \nActual " + actual );
+        }
+
+    }
+    public void assertEquals(double expected, double actual) {
+        if(expected == actual) {
+            passed++;
+        } else {
+            failed++;
+            System.out.println("Expected " + expected + " \nActual " + actual );
+        }
+    }
+    public void assertEquals(boolean expected, boolean actual) {
+        if(expected == actual) {
+            passed++;
+        } else {
+            failed++;
+            System.out.println("Expected " + expected + " \nActual " + actual );
+        }
     }
 }
--- test/TestClass.java
+++ test/TestClass.java
@@ -1,14 +1,35 @@
 public class TestClass extends TestSuite {
     @Override
     protected void testList() {
-        testThis();
-        tesThat();
+        testIntPassed();
+        testIntFailled();
+        testBoleanPassed();
+        testBoleanFailled();
+        testDoublePassed();
+        testDoubleFailled();
     }
 
-    public void testThis(){
-        //TODO
+    private void testDoublePassed() {
+        double result = 5.0;
+        assertEquals(5.0,result);
     }
-    public void tesThat(){
-        //TODO
+    private void testDoubleFailled(){
+        assertEquals(2.0,3.0);
     }
+
+    public void testIntPassed(){
+        int result = 5;
+        assertEquals(5,result);
+
+    }
+    public void testIntFailled(){
+        assertEquals(3,7);
+    }
+    public void testBoleanPassed() {
+        assertEquals(true, true);
+    }
+    public void testBoleanFailled() {
+        assertEquals(true,false);
+    }
+
 }
